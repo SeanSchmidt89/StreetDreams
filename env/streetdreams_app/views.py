6226f3ff57502cc8569d3607413ed0e45b92df51
sean <seanschmidt89@yahoo.com>
2022-05-23 05:00:19 -0700

    finished api page and view

--- env/streetdreams_app/views.py
+++ env/streetdreams_app/views.py
@@ -70,99 +70,36 @@ class Register(FormView):
 
 
 def Api(request):
-    vin = ''
     if 'car' in request.POST:
         vin = request.POST['car']
         api_key = 'ZrQEPSkKc2VhbnNjaG1pZHQ4OUB5YWhvby5jb20='
         url = 'https://auto.dev/api/vin/'+vin+'?apikey='+api_key 
         api_data = requests.get(url)
         json_data = api_data.json()
-        print(json_data)
         make = json_data['make']['name']
         model = json_data['model']['name']
+        engine = json_data['engine']['configuration']
+        cylinder = json_data['engine']['cylinder']
+        compressortype = json_data['engine']['compressorType']
+        year = json_data['years'][0]['year']
+        type = json_data['categories']['vehicleStyle']
+        horsepower = json_data['engine']['horsepower']
+        highway = json_data['mpg']['highway']
+        city = json_data['mpg']['city']
+        
         context = {
             'make': make,
             'model': model,
+            'engine': engine,
+            'cylinder': cylinder,
+            'compressortype': compressortype,
+            'year': year,
+            'type': type,
+            'horsepower': horsepower,
+            'highway': highway,
+            'city': city,
         }
     else:
         context = {}
-    
-    
-    
 
-    return render(request, 'streetdreams_app/api.html', context)
-
-
-
-
-    """    {
-        'make': {
-            'id': 200003381,
-            'name': 'Toyota',
-            'niceName': 'toyota'
-            }, 
-        'model': {
-            'id': 'Toyota_Supra',
-            'name': 'Supra',
-            'niceName': 'supra'
-            },
-        'engine': {
-            'id': '200503534',
-            'name': 'Engine',
-            'equipmentType': 'ENGINE',
-            'availability': 'STANDARD',
-            'cylinder': 6,
-            'size': 3,
-            'configuration': 'inline',
-            'fuelType': 'regular unleaded',
-            'horsepower': 320,
-            'torque': 315,
-            'type': 'gas',
-            'code': '6ITTG3.0',
-            'compressorType':
-            'twin turbocharger',
-            'rpm': {'horsepower': 5600, 'torque': 4000}
-            },
-        'transmission': {
-            'id': '200503532', 
-            'name': '6M', 
-            'equipmentType': 'TRANSMISSION', 
-            'availability': 'STANDARD', 
-            'transmissionType': 'MANUAL', 
-            'numberOfSpeeds': '6'}, 
-        'drivenWheels': 'rear wheel drive', 
-        'numOfDoors': '2', 
-        'options': [], 
-        'colors': [], 
-        'price': {
-            'usedTmvRetail': 30742, 
-            'usedPrivateParty': 22286, 
-            'usedTradeIn': 17212, 
-            'estimateTmv': False
-            }, 
-        'categories': {
-            'primaryBodyType': 'Car', 
-            'market': 'Performance', 
-            'epaClass': 'MINI_COMPACT_CARS', 
-            'vehicleSize': 'Compact', 
-            'vehicleType': 'Car', 
-            'vehicleStyle': 'Coupe'}, 
-        'squishVin': 'JT2JA82JP0', 
-        'years':[
-                {'id': 464, 
-                'year': 1993, 
-                'styles': [
-                    {'id': 8890, 
-                    'name': 'Turbo 2dr Coupe', 
-                    'submodel': {
-                        'body': 'Coupe', 
-                        'modelName': 'Supra Coupe', 
-                        'niceName': 'coupe'}, 
-                        'trim': 'Base'
-                    }
-                        ]
-                }
-                    ], 
-            'matchingType': 'SQUISHVIN', 
-            'mpg': {'highway': '22', 'city': '15'}}
-[22/May/2022 23:46:37] "POS"""
+    return render(request, 'streetdreams_app/vininfo.html', context)
